57d82c99e24e0c1150f75f2f4db6d899f2dd4e51
MichaelIllescas <joni.illes@hotmail.com>
2025-02-27 04:55:20 -0300

    Initial project structure

---
+++ inventioryApp/src/main/java/com/imperial_net/inventioryApp/InventioryAppApplication.java
@@ -0,0 +1,13 @@
+package com.imperial_net.inventioryApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventioryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventioryAppApplication.class, args);
+	}
+
+}
---
+++ inventioryApp/src/test/java/com/imperial_net/inventioryApp/InventioryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imperial_net.inventioryApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventioryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
